6fb4fd1b106616e601cfe01f86e29fe025f20dc7
c0d3sw0t <codeswot@proton.me>
2024-12-20 16:44:30 +0100

    remove untracked files

--- lib/mooncake.dart
+++ lib/mooncake.dart
@@ -6,7 +6,8 @@ import 'package:mooncake/src/mooncake_view.dart';
 class Mooncake {
   static Future<String?> show(BuildContext context) async {
     final result = await showDialog<String?>(context: context, builder: (context) => const MooncakeView());
-
+  
     return result;
   }
 }
+
