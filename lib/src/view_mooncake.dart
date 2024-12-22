fe881a64d082b92c0fd27a4b3ce498832a060b6c
c0d3sw0t <codeswot@proton.me>
2024-12-22 04:49:43 +0100

    method to show mooncake view

--- lib/src/view_mooncake.dart
+++ lib/src/view_mooncake.dart
@@ -1,3 +1,6 @@
-Future<String> startMooncake() async {
-  return '🌕';
+import 'package:flutter/material.dart';
+import 'package:mooncake/src/widgets/mooncake_view.dart';
+
+Future<String?> startMooncake(BuildContext context) async {
+  return await showDialog<String?>(context: context, builder: (context) => const MooncakeView());
 }
