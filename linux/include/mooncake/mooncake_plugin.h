3d7575b5cc22b960f2194bb50b6fc7b97c08c61a
flatFeather <github.astronaut143@silomails.com>
2024-12-28 23:11:14 +0100

    Fix linux plugin

--- linux/include/mooncake/mooncake_plugin.h
+++ linux/include/mooncake/mooncake_plugin.h
@@ -2,13 +2,24 @@
 #define FLUTTER_PLUGIN_MOONCAKE_PLUGIN_H_
 
 #include <flutter_linux/flutter_linux.h>
-#include <glib-object.h>
 
 G_BEGIN_DECLS
 
-G_DECLARE_FINAL_TYPE(MooncakePlugin, mooncake_plugin, MOONCAKE, PLUGIN, GObject)
+#ifdef FLUTTER_PLUGIN_IMPL
+#define FLUTTER_PLUGIN_EXPORT __attribute__((visibility("default")))
+#else
+#define FLUTTER_PLUGIN_EXPORT
+#endif
 
-void mooncake_plugin_register_with_registrar(FlPluginRegistrar* registrar);
+typedef struct _MooncakePlugin MooncakePlugin;
+typedef struct {
+  GObjectClass parent_class;
+} MooncakePluginClass;
+
+FLUTTER_PLUGIN_EXPORT GType mooncake_plugin_get_type();
+
+FLUTTER_PLUGIN_EXPORT void mooncake_plugin_register_with_registrar(
+    FlPluginRegistrar* registrar);
 
 G_END_DECLS
 
